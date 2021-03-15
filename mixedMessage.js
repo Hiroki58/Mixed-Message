10f098a2a26f260512665b255357b05816e62595
Hiroki <gnarly.hiro.15@gmail.com>
2021-03-15 15:11:29 +0900

    code fix

---
+++ mixedMessage.js
@@ -0,0 +1,9 @@
+const m1 =['banana','strawberry','chocolate'];
+const m2 =['juice','cookie','cake'];
+const m3 =['love','hate','like'];
+
+
+const mixedMessage = ()=>{
+  let num =  Math.floor(Math.random()*3);
+  console.log(`The flavor of this ${m2[num]} is ${m1[num]} and I ${m3[num]} it`);
+}
