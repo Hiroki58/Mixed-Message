308dd59e105829438da0b9be6b732762be1c2ca7
Hiroki <gnarly.hiro.15@gmail.com>
2021-03-15 15:18:50 +0900

    code fixed

--- mixedMessage.js
+++ mixedMessage.js
@@ -5,5 +5,7 @@ const m3 =['love','hate','like'];
 
 const mixedMessage = ()=>{
   let num =  Math.floor(Math.random()*3);
-  console.log(`The flavor of this ${m2[num]} is ${m1[num]} and I ${m3[num]} it`);
+  console.log(`The flavor of this ${m2[num]} is ${m1[num]} and I ${m3[num]} it!`);
 }
+
+mixedMessage();
